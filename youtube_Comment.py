8face36b2da109acbf7516d6fd0068f8ef433857
Payam Askarpour <39825165+payam59@users.noreply.github.com>
2024-02-03 18:42:47 -0500

    updated features
    now will save output with video names, also will fix invalid characters if found in the video name.

--- youtube_Comment.py
+++ youtube_Comment.py
@@ -1,5 +1,7 @@
 import argparse
 from googleapiclient.discovery import build
+import os
+import re
 
 # Function to get comment replies
 def get_comment_replies(youtube, parent_id):
@@ -54,27 +56,52 @@ def get_video_comments(youtube, video_id):
 
     return comments
 
+# Function to sanitize a string for use as a filename
+def sanitize_filename(filename):
+    # Remove invalid characters using a regular expression
+    return re.sub(r'[\/:*?"<>|]', '_', filename)
+
+
 def main():
     parser = argparse.ArgumentParser(description='YouTube Comments and Replies Fetcher')
     parser.add_argument('video_id', help='ID of the YouTube video for which to fetch comments and replies')
-    parser.add_argument('--apikey', help='Your YouTube Data API v3 key', default='')
-    parser.add_argument('output_file', help='File to write the comments and replies to')
+    parser.add_argument('--apikey', help='Your YouTube Data API v3 key', 
+                        default='Your_API_Key')
+    parser.add_argument('output_path', help='Path where the output file will be saved')
 
     args = parser.parse_args()
 
     # Build a resource object
     youtube = build('youtube', 'v3', developerKey=args.apikey)
 
+    # Fetch video details to get the video title
+    video_response = youtube.videos().list(
+        part='snippet',
+        id=args.video_id
+    ).execute()
+
+    if 'items' in video_response:
+        video_title = video_response['items'][0]['snippet']['title']
+    else:
+        print('Video not found.')
+        return
+
+    sanitized_video_title = sanitize_filename(video_title)
+    # Generate the output file name based on the video title
+    output_file_name = f"{sanitized_video_title}.txt"
+    output_file_path = os.path.join(args.output_path, output_file_name)
+
     # Fetch comments and replies
     video_comments = get_video_comments(youtube, args.video_id)
 
-    # Write comments and replies to the specified file
-    with open(args.output_file, 'w', encoding='utf-8') as file:
+    # Write comments and replies to the generated file
+    with open(output_file_path, 'w', encoding='utf-8') as file:
         for comment in video_comments:
             file.write(comment + '\n')
 
     # Print status update
     print(f'Total comments and replies fetched: {len(video_comments)}')
+    print(f'Comments and replies saved to: {output_file_path}')
 
 if __name__ == "__main__":
     main()
